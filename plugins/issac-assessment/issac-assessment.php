b286c86865ff7d787c66b65e77c2fa9a60508bdf
carlostalaga <cat@carlostalaga.com.au>
2026-06-14 17:49:59 +0930

    arranque

---
+++ plugins/issac-assessment/issac-assessment.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: ISSAC Assessment
+ * Description: Inclusive Schools Self-Assessment Checklist platform.
+ * Version:     0.1.0
+ * Requires PHP: 8.1
+ * Author:      Milacku
+ */
+defined('ABSPATH') || exit;
+require_once __DIR__ . '/vendor/autoload.php';
